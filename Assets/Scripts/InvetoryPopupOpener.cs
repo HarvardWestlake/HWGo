96bf5e15edb7c88c513a536643ec37a8fef2b579
Jingjing <chrystalduan@gmail.com>
2024-01-20 18:04:59 -0800

    panel opener code

---
+++ Assets/Scripts/InvetoryPopupOpener.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvetoryPopupOpener : MonoBehaviour {
+    public GameObject Panel;
+
+    public void OpenPanel() {
+        if(Panel != null) {
+            Panel.SetActive(true);
+        }
+    }
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+/*using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class UIManager : MonoBehaviour
@@ -39,4 +39,4 @@ public class UIManager : MonoBehaviour
             }
         }
     }
-}
+}*/
