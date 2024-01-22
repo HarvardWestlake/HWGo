ba80dcf69acb1fd6809eed908a05138abdaf1bf2
Wyatt Van Amburg <wyatt.va@icloud.com>
2024-01-21 18:01:09 -0800

    Stupid errors that aren't real (im in denial?)

--- Assets/Scenes/Settings/MusicSlider.cs
+++ Assets/Scenes/Settings/MusicSlider.cs
@@ -4,14 +4,14 @@ using UnityEngine.Audio;
 
 public class MusicSliderScript : MonoBehaviour
 {
-    public AudioMixer AudioMixer; // Assign your AudioMixer in the inspector
+    public AudioMixer audioMixer; // Assign your AudioMixer in the inspector
     private Slider musicSlider;
 
     void Start()
     {
         musicSlider = GetComponent<Slider>();
         float volume;
-        AudioMixer.GetFloat("MusicParam", out volume); // Get the current volume from the AudioMixer
+        audioMixer.GetFloat("MusicParam", out volume); // Get the current volume from the AudioMixer
         musicSlider.value = Mathf.Pow(10, volume / 20); // Convert the logarithmic value to linear slider value
         musicSlider.onValueChanged.AddListener(SetMusicVolume);
     }
@@ -19,6 +19,6 @@ public class MusicSliderScript : MonoBehaviour
     public void SetMusicVolume(float value)
     {
         // Convert the slider value to a logarithmic value that the mixer expects
-        AudioMixer.SetFloat("MusicParam", Mathf.Log10(value) * 20);
+        audioMixer.SetFloat("MusicParam", Mathf.Log10(value) * 20);
     }
 }
