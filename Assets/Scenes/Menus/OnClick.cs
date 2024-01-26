3285bcf18dbd357be7b4b69ff6c372ceff7060d2
Sarah <forsugar88@gmail.com>
2024-01-25 16:43:17 -0800

    small error fixed

--- Assets/Scenes/Menus/OnClick.cs
+++ Assets/Scenes/Menus/OnClick.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -13,80 +14,6 @@ public class OnClick : MonoBehaviour
         AudioListener.volume = 1;
     }
 
-     [Serializable]
 
-    public struct UserCredentials {
-
-        public string username;
-
-        public string password;
-
-    }
-
- 
-
-    void CreateAccount(string sUN, string sPW) {
-
-        UserCredentials objUserData = new UserCredentials();
-
-        objUserData.username = sUN;
-
-        objUserData.password = sPW;
-
-        string jsonUserData = JsonUtility.ToJson(objUserData);
-
-        Debug.Log(jsonUserData);
-
-        StartCoroutine(PostFileFromServer("createAccount", jsonUserData, CreateAccountCallback));
-
-    }
-
- 
-
-    // sData is user counting id
-
-    void CreateAccountCallback(string sData) {
-
-        if (null == sData) {
-
-            Debug.Log("Server error.");
-
-            return;
-
-        }
-
-        Debug.Log(sData);
-
-    }
-
- 
-
-    IEnumerator PostFileFromServer(string sField, string jsonData, System.Action<string> doneCallback) {
-
-        WWWForm form = new WWWForm();
-
-        form.AddField(sField, jsonData);
-
-        string sURL = "https://hwgo.codehw.net/main.php";
-
-        using (UnityWebRequest webRequest = UnityWebRequest.Post(sURL, form)) {
-
-            yield return webRequest.SendWebRequest();
-
-            if (webRequest.result == UnityWebRequest.Result.ConnectionError || webRequest.result == UnityWebRequest.Result.ProtocolError) {
-
-                Debug.LogError("Error: " + webRequest.error);
-
-                doneCallback?.Invoke(null);
-
-            }
-
-            else
-
-                doneCallback?.Invoke(webRequest.downloadHandler.text);
-
-        }
-
-    }
 
 }
