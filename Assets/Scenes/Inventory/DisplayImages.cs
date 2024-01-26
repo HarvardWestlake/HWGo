916b8b621329d5b0798695fe1bed99610af2f9ab
Oren Hartstein <orenh2020@gmail.com>
2024-01-26 10:49:28 -0800

    displayChanges

--- Assets/Scenes/Inventory/DisplayImages.cs
+++ Assets/Scenes/Inventory/DisplayImages.cs
@@ -4,23 +4,23 @@ using UnityEngine.UI;
 
 public class InventoryButtonManager : MonoBehaviour
 {
-    public List<Button> buttons; // Assign these buttons in the Unity Editor
+    public List<Image> buttonImages; // Assign the Images in the Unity Editor
 
-    public void UpdateButtons(List<int> inventory)
+    public void UpdateButtonImages(List<int> inventory)
     {
-        // First, disable all button images
-        foreach (var button in buttons)
+        // First, disable all images
+        foreach (var image in buttonImages)
         {
-            button.gameObject.SetActive(false);
+            image.enabled = false;
         }
 
         // Now, enable images for buttons in the inventory
         foreach (var item in inventory)
         {
             int index = item - 1; // Assuming inventory items start at 1
-            if (index >= 0 && index < buttons.Count)
+            if (index >= 0 && index < buttonImages.Count)
             {
-                buttons[index].gameObject.SetActive(true);
+                buttonImages[index].enabled = true;
             }
         }
     }
